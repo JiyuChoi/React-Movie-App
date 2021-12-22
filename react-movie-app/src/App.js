c9174aa8efcc0d95833c75db76ddd960dd45c88f
JiyuChoi <jiyou126@naver.com>
2021-12-22 15:36:49 +0900

    Create & Destroyed Components

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -3,30 +3,33 @@ import styles from "./App.module.css"
 
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
   useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("I run when 'keyword' changes.");
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  })
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
     }
-  }, [keyword]); // keyword가 변할 때만 실행됨
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]); // counter가 변할 때만 실행됨
-  useEffect(() => {
-    console.log("I run when 'keyword & counter' changes.");
-  }, [keyword, counter]); // 두 중 하나의 state가 변할 때
+  }, []);
+  return <h1>Hello</h1>
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input type="text" value={keyword} onChange={onChange} placeholder='Search here...'></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
