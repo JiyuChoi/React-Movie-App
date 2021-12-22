fe4ff45becefcbd16a885b4f366ecf175b781a48
JiyuChoi <jiyou126@naver.com>
2021-12-22 11:54:59 +0900

    Using useEffect

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,11 +1,32 @@
 import Button from "./Button";
 import styles from "./App.module.css"
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]); // keyword가 변할 때만 실행됨
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]); // counter가 변할 때만 실행됨
+  useEffect(() => {
+    console.log("I run when 'keyword & counter' changes.");
+  }, [keyword, counter]); // 두 개의 state가 변할 때
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Continue"} />
+      <input type="text" value={keyword} onChange={onChange} placeholder='Search here...'></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
