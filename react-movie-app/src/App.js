bec73a367cf12e0d4976fbaa74ee90b1b9873536
JiyuChoi <jiyou126@naver.com>
2021-12-25 22:22:15 +0900

    Create Coin exchanger

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [currency, setCurrency] = useState();
+  const [value, setValue] = useState(0);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -11,20 +13,35 @@ function App() {
         setLoading(false);
       });
   }, [])
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+  const onSelect = (event) => {
+    setCurrency(event.target.value);
+  }
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ?
         (<strong>Loading...</strong>) :
-        (<select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>)}
+        (<div>
+          <select value={currency} onChange={onSelect}>
+            <option>Select currency</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol})
+              </option>
+            ))}
+          </select>
+          <div>
+            <input type="text" placeholder="Write value" onChange={onChange} />
+            <h3>{currency ? value / currency : 0}</h3>
+          </div>
+        </div>)
+      }
 
-    </div >
+
+    </div>
   );
 }
 
