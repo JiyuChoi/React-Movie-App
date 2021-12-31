d65b1aa52082ce6b1a5b5713ff384826807f043b
JiyuChoi <jiyou126@naver.com>
2021-12-31 23:57:36 +0900

    Create Loading

--- react-movie-app/src/routes/Detail.js
+++ react-movie-app/src/routes/Detail.js
@@ -1,16 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 function Detail() {
     const { id } = useParams();
+    const [loading, setLoading] = useState(true);
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
-        console.log(json);
+        console.log(json.data.movie);
+        setLoading(false);
     };
     useEffect(() => {
         getMovie();
     }, []);
-    return <h1>Detail</h1>;
+    return (<h1>Detail</h1>);
 }
 export default Detail;
